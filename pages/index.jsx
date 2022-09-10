9240cd0a2a4f3fa4ad96a559e5cf09ca8271f80a
E-K8 <ekatplat@gmail.com>
2022-09-10 20:30:03 +0100

    create counter component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,27 @@
 import { useState } from 'react';
 import styles from './index.module.scss';
 
+const Counter = ({ count, setCount }) => (
+  <div className={styles.counter}>
+    <div className={styles.count}>{count} </div>
+    <div className={styles.button} onClick={() => setCount(count + 1)}>
+      +
+    </div>
+    <div
+      className={`${styles.button}+${styles.resetButton}`}
+      onClick={() => setCount(0)}
+    >
+      RESET
+    </div>
+  </div>
+);
+
 export default function Home() {
   const [count, setCount] = useState(0);
 
   return (
     <div className={styles.container}>
-      <div className={styles.counter}>
-        <div className={styles.count}>{count} </div>
-        <div className={styles.button} onClick={() => setCount(count + 1)}>
-          +
-        </div>
-        <div
-          className={`${styles.button}+${styles.resetButton}`}
-          onClick={() => setCount(0)}
-        >
-          RESET
-        </div>
-      </div>
+      <Counter count={count} setCount={setCount} />
     </div>
   );
 }
